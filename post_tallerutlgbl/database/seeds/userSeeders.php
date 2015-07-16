0892fbe3383d1157bfa7d9c5e14f9b84b624477e
GerardoZ <gerardozarate.p@gmail.com>
2015-07-16 15:20:23 -0500

    Taller

---
+++ post_tallerutlgbl/database/seeds/userSeeders.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: tallerutl
+ * Date: 16/07/15
+ * Time: 01:37 PM
+ */
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use Faker\Factory as Faker;
+class userSeeders extends Seeder
+{
+    public function run(){
+        $faker = Faker::create();
+        for($i = 0; $i < 100; $i++)
+        {
+            $id = \DB::table('users')->insertGetId(array(
+               'name' => $faker->firstName,
+                'email' =>$faker->unique()->email,
+                'password' => \Hash::make('123456'),
+                'alias' => $faker->userName,
+            ));
+
+
+        }
+}
+
+}
---
+++ post_tallerutlgbl/resources/views/users/index.blade.php
@@ -0,0 +1,16 @@
+@extends ('app')
+@section ('content')
+    <table>
+        <tr>
+            <th>Nombre</th>
+        </tr>
+
+            @foreach($users as $user)
+            <tr>
+                <td>{{$user->name}}</td>
+            </tr>
+            @endforeach
+
+    </table>
+    {!! $users->render() !!}
+@endsection
